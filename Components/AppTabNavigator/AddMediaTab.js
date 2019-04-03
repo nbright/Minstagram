b3438162c660931ff4164452533cc4f95ddb5556
nbright <kms3700@gmail.com>
2019-04-03 19:03:36 +0900

    아이콘추가

--- Components/AppTabNavigator/AddMediaTab.js
+++ Components/AppTabNavigator/AddMediaTab.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 
 export default class AddMediaTab extends Component {
+    static navigationOptions = {
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name='ios-add-circle' style={{ color: tintColor }} />
+        )
+    }
     render() {
         return (
             <View style={style.container}>
--- Components/AppTabNavigator/ProfileTab.js
+++ Components/AppTabNavigator/ProfileTab.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
 
 export default class ProfileTabTab extends Component {
+    static navigationOptions = {
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name='person' style={{ color: tintColor }} />
+        )
+    }
     render() {
         return (
             <View style={style.container}>
